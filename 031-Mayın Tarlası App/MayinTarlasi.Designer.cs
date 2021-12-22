9f03c983a7bd3fc98161e57e7b603948a18df3e0
Emre <emre-karamahmut-@hotmail.com>
2021-12-23 00:16:58 +0300

    !

---
+++ 031-Mayın Tarlası App/MayinTarlasi.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace _031_Mayın_Tarlası_App
+{
+    partial class MayinTarlasi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MayinTarlasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MayinTarlasi";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MayinTarlasi_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 031-Mayın Tarlası App/MayinTarlasi.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _031_Mayın_Tarlası_App
+{
+    public partial class MayinTarlasi : Form
+    {
+        public MayinTarlasi()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        private void MayinTarlasi_Load(object sender, EventArgs e)
+        {
+            List<int> mayinlar = new List<int>();
+            //oluşturlan tüm butonlar özelinde 20 adet mayın belirlemeye çalıştık.
+            //Rasgele berlirlenen 20 sayıyı ,oluşan numaraları picbox'lara mayın olarak atayayacağız
+            for (int i = 0; i < 20; i++)
+            {
+                int mayin = rnd.Next(0, 160);
+                if (!mayinlar.Contains(mayin))
+                {
+                    mayinlar.Add(mayin);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            for (int i = 0; i < 169; i++)
+            {
+                PictureBox pcb = new PictureBox();
+                pcb.Name = "pcb" + i;
+                pcb.Width = 30;
+                pcb.Height = 30;
+                pcb.Margin = new Padding(1);
+                pcb.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+                if (mayinlar.Contains(i))
+                {
+                    pcb.Tag = true;
+
+                }
+                else
+                {
+                    pcb.Tag = false;
+                }
+                pcb.Click += Pcb_Click;
+                flowLayoutPanel1.Controls.Add(pcb);
+
+            }
+            timer1.Start();
+        }
+
+        private void Pcb_Click(object sender, EventArgs e)
+        {
+            PictureBox pcb = (PictureBox)sender;
+
+            Color background = pcb.BackColor;
+
+            if ((bool)pcb.Tag)
+            {
+                timer1.Stop();
+                foreach (var item in flowLayoutPanel1.Controls)
+                {
+                    pcb = (PictureBox)item;
+                    pcb.BackColor = background;
+                }
+                MessageBox.Show("Mayın'a BASTINIZ!");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var item in flowLayoutPanel1.Controls)
+            {
+                //panel içerisinde sadece picturebox yer aldığından ,kontrol etmemize gerek yoktur.
+                PictureBox pcb = (PictureBox)item;
+                pcb.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            }
+        }
+    }
+}
+
